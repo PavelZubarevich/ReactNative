6355a93b96fcd61aee09848feda035dc4da7dfa8
PavelZ <pavelzubarevich@gmail.com>
2021-09-17 13:34:47 +0300

    linter

--- custom.d.ts
+++ custom.d.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 declare module '*.jpg';
 declare module '*.png';
 declare module '*.jpeg';
 declare module '*.gif';
-=======
-declare module '*.png';
->>>>>>> 635be3855c58f852e964f9f1059a209b0b76c686
