b44e2428e14dce0447ca085687c72607ab3b53b0
PavelZ <pavelzubarevich@gmail.com>
2021-09-26 21:03:29 +0300

    add profile button to avatar modal

--- components/LogOutModal.tsx
+++ components/LogOutModal.tsx
@@ -7,10 +7,10 @@ import {
   TouchableOpacity,
   Text,
   Dimensions,
-  Platform,
 } from 'react-native';
 import {useDispatch} from 'react-redux';
 import {logOut} from '../redux/actionCreators/actionCreators';
+import colors from '../colors/colors';
 
 interface IModal {
   showModal: boolean;
@@ -22,13 +22,17 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 export const LogOutModal: FC<IModal> = ({showModal, modalHandler}) => {
   const dispatch = useDispatch();
 
-  const {background, modal, textStyle} = styles;
+  const {background, modal, textStyle, divider} = styles;
   return (
     <Modal transparent visible={showModal} animationType="fade">
       <TouchableWithoutFeedback onPress={modalHandler}>
         <View style={background} />
       </TouchableWithoutFeedback>
       <View style={modal}>
+        <TouchableOpacity onPress={() => console.log('prof')}>
+          <Text style={textStyle}>Profile</Text>
+        </TouchableOpacity>
+        <View style={divider} />
         <TouchableOpacity onPress={() => dispatch(logOut())}>
           <Text style={textStyle}>Log out</Text>
         </TouchableOpacity>
@@ -40,20 +44,27 @@ export const LogOutModal: FC<IModal> = ({showModal, modalHandler}) => {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    backgroundColor: 'rgba(0, 0, 0, 0.03)',
   },
   modal: {
     position: 'absolute',
     right: 10,
-    top: Platform.OS === 'ios' ? '13%' : '11%',
+    top: SCREEN_WIDTH > 576 ? 95 : SCREEN_WIDTH > 386 ? 80 : 65,
     backgroundColor: '#fff',
-    width: SCREEN_WIDTH > 576 ? 120 : 80,
-    height: SCREEN_WIDTH > 576 ? 75 : 50,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 3,
+    borderRadius: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
   textStyle: {
-    fontSize: SCREEN_WIDTH > 576 ? 20 : 13,
+    fontSize: SCREEN_WIDTH > 576 ? 20 : 14,
+    color: colors.darkGrey,
+  },
+  divider: {
+    width: '100%',
+    height: 1,
+    backgroundColor: colors.lightGrey,
+    marginVertical: 8,
   },
 });
