5526779f65ba945b326ee851eab1fe65e2985b52
Pavel Zubarevich <pavel.zubarevich@oracle.com>
2021-11-24 20:13:05 +0300

    feat: add splash screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
+// import {Platform, StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigator from './navigator';
 import {Provider} from 'react-redux';
 import {store, persistor} from './redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: FC = () => {
+  useEffect(() => {
+    setTimeout(() => SplashScreen.hide(), 1000);
+  }, []);
+
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaProvider>
-          <Navigator />
-        </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaProvider>
+            <Navigator />
+          </SafeAreaProvider>
+        </PersistGate>
+      </Provider>
+    </>
   );
 };
 
