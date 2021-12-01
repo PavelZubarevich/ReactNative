c0a093f05fac4290982d6323b185a566cf8a9e29
Pavel Zubarevich <pavel.zubarevich@oracle.com>
2021-12-01 18:04:11 +0300

    mearge fix

--- Theme/index.ts
+++ Theme/index.ts
@@ -1,11 +1,7 @@
 export {AppHeader} from './AppHeader';
-<<<<<<< HEAD
-export {AppHeader as app} from './AppHeader';
-=======
 export {CustomInput} from './CustomInput';
 export {OvalSolidButton} from './OvalSolidButton';
 export {OvalOutlinedButton} from './OvalOutlinedButton';
 export {TextButton} from './TextButton';
 export {OvalTextInput} from './OvalTextInput';
 export {SearchBarCustom as SearchBar} from './SearchBar';
->>>>>>> master
